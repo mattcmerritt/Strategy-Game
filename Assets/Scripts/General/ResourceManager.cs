cdab29569388cdf9b0ea2a339db8fb9ad0843bed
mattcmerritt <mattcmerritt@gmail.com>
2023-05-12 09:25:15 -0400

    berries now spawn on the terrain

--- Assets/Scripts/General/ResourceManager.cs
+++ Assets/Scripts/General/ResourceManager.cs
@@ -5,7 +5,13 @@ using TMPro;
 
 public class ResourceManager : MonoBehaviour
 {
-    [SerializeField] private Dictionary<Resource, int> ResourcesCollected;
+    [SerializeField] private Dictionary<Resource, int> ResourcesCollected = new Dictionary<Resource, int>();
+
+    private void Start()
+    {
+        ResourcesCollected[Resource.Food] = 0;
+        ResourcesCollected[Resource.Wood] = 0;
+    }
 
     public void AddResources(Resource type, int additional)
     {
