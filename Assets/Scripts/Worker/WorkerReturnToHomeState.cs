6faf966136db73f75bcbc3b15abb6215b2835fe3
mikepmerritt <mikepmerritt@gmail.com>
2023-05-12 05:29:08 -0400

    workers now treat closest building as home for dropoff/retreating

--- Assets/Scripts/Worker/WorkerReturnToHomeState.cs
+++ Assets/Scripts/Worker/WorkerReturnToHomeState.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public class WorkerReturnToHomeState : AgentState
 {
-    private float MinDistanceFromHome = 0.25f;
+    private float MinDistanceFromHome = 4f;
 
     public override void ActivateState(Agent agent)
     {
+        Vector3 closestBuildingPos = ((Worker)agent).FindClosestBuilding().transform.position;
+        ((Worker)agent).SetHomePosition(closestBuildingPos);
         Debug.Log("WORKER: Returning home now");
         agent.GetNavAgent().SetDestination(((Worker)agent).GetHomePosition());
     }
@@ -16,6 +18,7 @@ public class WorkerReturnToHomeState : AgentState
     {
         // If sufficiently close to home, drop off resources and go back out
         Vector3 home = ((Worker)agent).GetHomePosition();
+        home.y = agent.transform.position.y;
         // Debug.Log("Distance to home: " + Vector3.Magnitude(home - agent.transform.position));
         if (Vector3.Magnitude(home - agent.transform.position) < MinDistanceFromHome)
         {
