d49f17a0652b1713f92d22d92d99b2f9c98aeba8
mikepmerritt <mikepmerritt@gmail.com>
2024-01-31 14:18:16 -0500

    modeling and animations for movement indicators

---
+++ Assets/Scripts/Unit Selection/ClickIndicator.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ClickIndicator : MonoBehaviour
+{
+    [SerializeField] private Canvas ArrowCanvas;
+
+    private void OnEnable()
+    {
+        // set event camera
+        ArrowCanvas = GetComponent<Canvas>();
+        ArrowCanvas.worldCamera = Camera.current;
+
+
+    }
+
+    private void Update()
+    {
+
+    }
+
+    public void EnableArrowCanvas()
+    {
+        ArrowCanvas.gameObject.SetActive(true);
+    }
+    public void DisableArrowCanvas()
+    {
+        ArrowCanvas.gameObject.SetActive(false);
+    }
+}
